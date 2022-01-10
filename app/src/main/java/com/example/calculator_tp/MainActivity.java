ee859bf9306516e2a07854df91fb255a8d01ab26
marwaneech <marwane.busmn@gmail.com>
2022-01-10 18:39:06 +0000

    1

--- app/src/main/java/com/example/calculator_tp/MainActivity.java
+++ app/src/main/java/com/example/calculator_tp/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.calculator_tp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Float Final_Value=0f;
+    EditText Text_box;
+    Float Last_Value=0f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Text_box= findViewById(R.id.Text_box);
     }
+
+    public void Clear(View view){
+
+        Final_Value=0f;
+        Last_Value=null;
+        Text_box.setText(null);
+
+
+    }
+    public void Result(View view){
+
+        Text_box.setText(Final_Value.toString());
+
+    }
+    public void Value_typed(View view , int num){
+        Final_Value+=num;
+
+    }
+
 }
